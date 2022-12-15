bf6a7e4b5820754784e372e4cf1bdbb82ec18c17
Mithilesh-create <mithileshsharma539@gmail.com>
2022-12-15 15:28:06 +0530

    added welcome page

---
+++ components/announcement/index.php
@@ -0,0 +1,31 @@
+<div class="announcement">
+        <div class="announcementTitle">
+            <p>Important Announcements:-</p>
+        </div>
+        <div class="messageDisplay">
+
+        </div>
+    </div>
+<style>
+    .messageDisplay{
+    background-color: #c4c4c4;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    min-height: 400px;
+    height: auto;
+}
+.announcementTitle>p{
+    color:#AF0000;
+    text-decoration: underline;
+    font-weight: bold;
+}
+.announcement{
+    padding: 15px;
+}
+@media only screen and (max-width: 600px) {
+    
+    .announcement{
+        padding: 5px;
+    }
+}
+</style>
--- pages/welcome/index.php
+++ pages/welcome/index.php
@@ -8,6 +8,18 @@ include_once("../../components/head.php");
     <?php
     include_once("../../components/navbar/index.php");
     ?>
+    <div class="internWelcome">
+        <div>
+            <p>Welcome to PCE Internship Portal</p>
+        </div>
+        <div class="loginBtn">
+            <a href="../login/index.php" class="btn btn-success btn-lg" role="button">Login</a>
+        </div>
+    </div>
+    <?php
+    include_once("../../components/announcement/index.php");
+    ?>
+
 </body>
 
 </html>
