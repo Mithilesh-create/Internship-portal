ffec02c4304944a63ddc7e432f0c1f4af7631a11
Mithilesh-create <mithileshsharma539@gmail.com>
2022-12-18 13:15:46 +0530

    test push

--- pages/staff/new.php
+++ pages/staff/new.php
@@ -17,17 +17,17 @@ include_once("../../components/head.php");
 
                 <div class="col-12">
 
-                    <strong for="CompanyName" class="form-label">Announcement Title</strong>
+                    <strong for="Title" class="form-label">Announcement Title</strong>
                     <br>
                     <br>
 
-                    <input type="text" class="form-control" spellcheck="false" required autocomplete="off"
-                        name="CompanyName" id="CompanyName" placeholder="e.g. ABC pvt. ltd.">
+                    <input type="text" class="form-control" spellcheck="false" required autocomplete="off" name="Title"
+                        id="Title" placeholder="e.g. ABC pvt. ltd. hiring interns for XYZ fields....">
                 </div>
                 <br>
 
                 <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" class="form-label">
+                    <label for="Description" class="form-label">
                         <strong>
                             Description
                         </strong>
@@ -35,79 +35,72 @@ include_once("../../components/head.php");
                     </label>
                     <br>
 
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"
-                        placeholder="e.g.Please collect the approval letter from office"></textarea>
+                    <textarea class="form-control" id="Description" rows="10"
+                        placeholder="Description Of Announcement"></textarea>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" class="form-label">
+                    <label for="skills" class="form-label">
                         <strong>
                             Skills required
                         </strong>
 
                     </label>
                     <br>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"
-                        placeholder="e.g.Please collect the approval letter from office"></textarea>
+                    <textarea class="form-control" id="skills" rows="2"
+                        placeholder="e.g. AutoCAD, JAVA, Web development, PCB Designing, etc..."></textarea>
                 </div>
                 <div class="col-12">
-
-                    <strong for="CompanyName" class="form-label">Company Location</strong>
+                    <strong for="Location" class="form-label">Location</strong>
                     <br>
 
                     <input type="text" class="form-control" spellcheck="false" required autocomplete="off"
-                        name="CompanyName" id="CompanyName" placeholder="e.g. ABC pvt. ltd.">
+                        name="Location" id="Location" placeholder="e.g. Raigad,Panvel">
                 </div>
                 <br>
+
                 <div class="col-12">
-                    <strong for="inputHeight" class="form-label">Start Date</strong>
+                    <strong for="startDate" class="form-label">Start Date</strong>
                     <input id="startDate" class="form-control" type="date" />
 
                 </div>
                 <br>
                 <div class="col-12">
 
-                    <strong for="CompanyName" class="form-label">Duration</strong>
+                    <strong for="Duration" class="form-label">Duration</strong>
                     <br>
 
                     <input type="text" class="form-control" spellcheck="false" required autocomplete="off"
-                        name="CompanyName" id="CompanyName" placeholder="e.g. ABC pvt. ltd.">
+                        name="Duration" id="Duration" placeholder="Number (In Months)">
                 </div>
                 <br>
-                <div class="col-12">
 
-                    <strong for="CompanyName" class="form-label">Stipend</strong>
-                    <br>
-
-                    <input type="text" class="form-control" spellcheck="false" required autocomplete="off"
-                        name="CompanyName" id="CompanyName" placeholder="e.g. ABC pvt. ltd.">
-                </div>
                 <div class="form-group">
                     <label><strong>Branch :</strong></label>
                     <br>
                     <br>
 
                     <label class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="" value="ECS" />
+                        <input class="form-check-input" type="radio" name="ECS" value="ECS" />
                         <span class="form-check-label">ECS</span>
                     </label>
                     <label class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="" value="CS" />
+                        <input class="form-check-input" type="radio" name="CS" value="CS" />
                         <span class="form-check-label">CS</span>
                     </label>
                     <label class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="" value="IT" />
+                        <input class="form-check-input" type="radio" name="IT" value="IT" />
                         <span class="form-check-label">IT</span>
                     </label>
                     <label class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="" value="MECH" />
+                        <input class="form-check-input" type="radio" name="MECH" value="MECH" />
                         <span class="form-check-label">MECH</span>
                     </label>
                     <label class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="" value="AUTO" />
+                        <input class="form-check-input" type="radio" name="AUTO" value="AUTO" />
                         <span class="form-check-label">AUTO</span>
                     </label>
                     <label class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="" value="AUTO" />
+                        <input class="form-check-input" type="radio" name="All" value="All" />
                         <span class="form-check-label">All Branches</span>
                     </label>
 
@@ -118,11 +111,11 @@ include_once("../../components/head.php");
                     <br>
 
                     <label class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="Stipend" value="Paid" />
+                        <input class="form-check-input" type="radio" name="WorkType" value="Paid" />
                         <span class="form-check-label"> Paid </span>
                     </label>
                     <label class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="Stipend" value="UnPaid" />
+                        <input class="form-check-input" type="radio" name="WorkType" value="UnPaid" />
                         <span class="form-check-label"> Unpaid </span>
                     </label>
                 </div>
@@ -132,19 +125,26 @@ include_once("../../components/head.php");
                     <br>
 
                     <label class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="Stipend" value="Paid" />
-                        <span class="form-check-label"> Lumpsum </span>
+                        <input class="form-check-input" type="radio" name="StipendType" value="Paid" />
+                        <span class="form-check-label"> Lumpsum (After Internship Duration)</span>
                     </label>
                     <label class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="Stipend" value="UnPaid" />
+                        <input class="form-check-input" type="radio" name="StipendType" value="UnPaid" />
                         <span class="form-check-label"> Monthly </span>
                     </label>
                     <label class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="Stipend" value="UnPaid" />
+                        <input class="form-check-input" type="radio" name="StipendType" value="UnPaid" />
                         <span class="form-check-label"> Unpaid </span>
                     </label>
                 </div>
+                <div class="col-12">
 
+                    <strong for="Stipend" class="form-label">Stipend</strong>
+                    <br>
+
+                    <input type="text" class="form-control" spellcheck="false" required autocomplete="off"
+                        name="Stipend" id="Stipend" placeholder="(In Rupees)">
+                </div>
                 <div class="form-group">
                     <label><strong>Work Location :</strong></label>
                     <br>
@@ -163,10 +163,18 @@ include_once("../../components/head.php");
                         <span class="form-check-label"> On-site </span>
                     </label>
                 </div>
+                <div class="col-12">
+                    <strong for="Perks" class="form-label">Perks</strong>
+                    <br>
+
+                    <input type="text" class="form-control" spellcheck="false" required autocomplete="off" name="Perks"
+                        id="Perks" placeholder="e.g. Certificate, Letter Of Recommendation, Flexible timings, etc...">
+                </div>
+                <br>
                 <div class="container text-center">
                     <div class="row mx-auto">
-                        <div class="col mt-3">
-                            <button class="btn btn-primary btn-lg col-md-12" role="button">Submit</button>
+                        <div class="col mt-5">
+                            <button class="btn btn-warning btn-lg col-md-12" role="button">Add Announcement</button>
                         </div>
 
                     </div>
